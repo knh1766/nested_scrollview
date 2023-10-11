45d3e35a1d7917576b4b63eee1b8c06015ad1ed2
metacoding <getinthere@naver.com>
2023-10-11 11:16:17 +0900

    netsted

---
+++ lib/pages/profile_page.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:profile_app/components/profile_buttons.dart';
+import 'package:profile_app/components/profile_count_info.dart';
+import 'package:profile_app/components/profile_drawer.dart';
+import 'package:profile_app/components/profile_header.dart';
+import 'package:profile_app/components/profile_tab.dart';
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      endDrawer: ProfileDrawer(),
+      appBar: AppBar(
+        leading: Icon(Icons.arrow_back_ios),
+        title: Text("Profile"),
+        centerTitle: true,
+      ),
+      body: NestedScrollView(
+        headerSliverBuilder: (context, innerBoxIsScrolled) {
+          return [
+            SliverList(
+              delegate: SliverChildListDelegate(
+                [
+                  ProfileHeader(),
+                  ProfileCountInfo(),
+                  ProfileButtons(),
+                ],
+              ),
+            )
+          ];
+        },
+        body: Expanded(child: ProfileTab()),
+      ),
+    );
+  }
+}
